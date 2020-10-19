9715ae20957ec1a9c4fcc9eedc84a0ee481461d2
BinongKBey <binongkbey1@gmail.com>
2020-10-19 18:17:33 +0530

    Auth small fix

--- Middleware/Auth.js
+++ Middleware/Auth.js
@@ -15,7 +15,7 @@ const auth = async (req, res, next) => {
         next()
     }
     catch (e) {
-        res.status(401).json({ error: "Please Login or Signup first" })
+        res.status(401).send()
     }
 }
 
